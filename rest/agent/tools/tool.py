bc9065e28b45c340e385e756df0a3866c199500d
JasonAlbertEinstien <j2004nol@gmail.com>
2025-08-20 23:31:59 +0800

    create tool setup

--- rest/agent/tools/tool.py
+++ rest/agent/tools/tool.py
@@ -1,24 +1,25 @@
 # abstract class of tool
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Any, Callable
 
 
 class Tool(ABC):
 
-    def __init__(self, ) -> None:
+    def __init__(self, **kwargs) -> None:
         self.tool: Callable
         self.description = ""
         self.parameters = {}
 
         # arguments should pass to run
         self.arguments = {}
+        self.values = {}
 
     @abstractmethod
-    def run(self):
+    def run(self) -> dict[str, Any]:
         pass
 
     @abstractmethod
-    def get_parameters(self):
+    def get_parameters(self) -> dict[str, Any]:
         pass
 
     def get_description(self):
