4fa12f9672ca3f1605b3b3c496547255ecc572c9
XinweiHe <xinwei.he.swe@gmail.com>
2025-10-19 17:32:37 -0700

    update

--- examples/distributed_services/multi_code_agent/ui/traceroot.config.ts
+++ examples/distributed_services/multi_code_agent/ui/traceroot.config.ts
@@ -1,25 +1,37 @@
-import type { TraceRootConfigFile } from 'traceroot-sdk-ts';
+import type { TraceRootConfigFile } from 'traceroot-sdk-ts/src/config';
 
 const config: TraceRootConfigFile = {
-  // Basic service configuration
+  // The token for the TraceRoot API
+  token: your_traceroot_token_here,
+
+  // The name of the service you are tracing.
   service_name: 'ts-example-service',
+
+  // The owner of the GitHub repository
   github_owner: 'traceroot-ai',
+
+  // The name of the GitHub repository
   github_repo_name: 'traceroot',
+
+  // The commit hash of the GitHub repository
   github_commit_hash: 'main',
 
-  // Your environment configuration
-  // development, staging, production
-  environment: 'development',
+  // Enable cloud export of spans (default: `true`)
+  enable_span_cloud_export: true,
 
-  // Token configuration
-  token: 'your_traceroot_token_here',
+  // Enable cloud export of logs (default: `true`)
+  enable_log_cloud_export: true,
 
-  // Whether to enable console export of spans and logs
+  // Enable console export of spans (default: `false`)
   enable_span_console_export: false,
+
+  // Enable console export of logs (default: `true`)
   enable_log_console_export: true,
 
-  // Local mode that whether to store all data locally
+  // The environment configuration such as development, staging, production
+  environment: 'development',
+
+  // Whether to store all telemetry data locally.
   local_mode: false,
 };
-
 export default config;
